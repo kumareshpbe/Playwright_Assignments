0287dc7d276e7aa44bd00a1ca0ee683cb485e074
Kumaresh <35972682+kumareshpbe@users.noreply.github.com>
2024-04-02 12:07:06 +0530

    Add files via upload
    Day1 Assignments

---
+++ Day 1/practise1.js
@@ -0,0 +1,17 @@
+let firstName = "Kumaresh"
+let companyName = "testleaf"
+let mobileNumber = 994284426
+let isAutomation = true
+let hasPlaywright 
+
+console.log(typeof firstName) 
+console.log (firstName)
+console.log(typeof companyName)
+console.log(companyName)
+console.log(typeof mobileNumber)
+console.log (mobileNumber)
+console.log (typeof isAutomation)
+console.log(isAutomation)
+console.log (typeof hasPlaywright)
+console.log(hasPlaywright)
+
---
+++ Day 1/practise2.js
@@ -0,0 +1,13 @@
+const browserVersion = "chrome"
+
+function getBrowserVersion ()
+{
+    if (browserVersion == "chrome")
+    {
+        let  browserVersion = "ios"
+        console.log (browserVersion)
+   }
+        console.log(browserVersion)
+}
+
+getBrowserVersion();
