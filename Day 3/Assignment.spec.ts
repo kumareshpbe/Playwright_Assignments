f614d7e2e394aa99c13d82c19844d47ab6f1ac99
Kumaresh <35972682+kumareshpbe@users.noreply.github.com>
2024-04-06 21:45:35 +0530

    Add files via upload
    Day 3 Assignment

---
+++ Day 3/Assignment.spec.ts
@@ -0,0 +1,29 @@
+import { test, chromium } from "@playwright/test";
+
+test("Test salesforce login", async () => {
+
+    //To launch the browser
+    const browser = await chromium.launch({ headless: false, channel: "chrome" });
+    //Create a browser context
+    const browserContext = await browser.newContext();
+    //Create a new page
+    const page = await browserContext.newPage();
+
+    await page.goto("https://login.salesforce.com/")
+    await page.fill('#username', 'kumareshpbe@gmail.com')
+    await page.fill('#password', 'Raani@26')
+    await page.click("#Login")
+    await page.waitForTimeout(10000)
+
+    console.log("Title: ",await page.title())
+    console.log("URL: ",page.url())
+
+
+    await page.close();
+    //Close the context
+    await browserContext.close();
+    //Close the browser instance
+    await browser.close();
+
+})
+
